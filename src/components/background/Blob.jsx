bec6b94b0edf63c2eccb8444f0e0cb76fb3392fe
Seb0927 <sebastianidrobo@hotmail.com>
2025-04-20 09:56:18 -0500

    feat(4.1.2): make blob inaccessible by making it readable for screen readers

--- src/components/background/Blob.jsx
+++ src/components/background/Blob.jsx
@@ -3,9 +3,7 @@ import blobSvg from '../../assets/vectors/blob.svg';
 const Blob = () => {
   return (
     <div>
-      { /* Justification:
-      https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Attributes/aria-hidden */}
-      <img src={blobSvg} aria-hidden="true" 
+      <img src={blobSvg} alt='vectors/blob.svg'
       className='w-80 h-80 md:h-108 md:w-108 lg:h-128 lg:w-128'/>
     </div>
   )
