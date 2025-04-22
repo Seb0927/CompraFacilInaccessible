f10e17aca6e661d6f8284eb3777450ba38efb66b
Seb0927 <sebastianidrobo@hotmail.com>
2025-04-21 20:34:51 -0500

    feat(2.1.4): add shortcut in catalog redirect to shopping cart - When the user presses the "c" key, it redirects to /payment/card

--- src/components/catalog/Catalog.jsx
+++ src/components/catalog/Catalog.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Item from './Item';
 import products from '@/utils/products';
 import { ShoppingCartProvider } from '../../contexts/ShoppingCartContext';
 import Music from './Music';
 
 const Catalog = () => {
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      // Check if the pressed key is 'c' or 'C'
+      if (event.key.toLowerCase() === 'c') {
+        window.location.href = '/payment/cart';
+      }
+    };
+
+    // Add event listener for keydown events
+    window.addEventListener('keydown', handleKeyDown);
+
+    // Clean up the event listener when the component unmounts
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, []);
+
   console.log(localStorage.getItem('shopping_cart'))
   return (
     <section>
@@ -32,6 +49,10 @@ const Catalog = () => {
           ))}
         </ShoppingCartProvider>
       </div>
+
+      <p className='text-center text-lg mt-4 py-6 bg-blue-medium-light border-4 border-blue-medium-dark rounded-lg'>
+        <span className='font-bold'>Consejo:</span> ¡Puedes utilizar la tecla "C" para ir a tu carrito de compras!
+      </p>
     </section>
   )
 }
