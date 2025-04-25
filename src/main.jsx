4e28cadf52a10f800d78215eeaaf24f463acdaee
Seb0927 <sebastianidrobo@hotmail.com>
2025-04-25 10:10:30 -0500

    feat(2.4.2): apply an absolute title to all page. - Set to "CompraFácil"

--- src/main.jsx
+++ src/main.jsx
@@ -38,21 +38,6 @@ const components = {
   payment: Payment
 }
 
-// Map component titles to their respective names
-const titles = {
-  catalog: 'Catalogo',
-  blog: 'Blog',
-  login: 'Iniciar Sesión',
-  register: 'Registro',
-  assistance: 'Asistencia',
-  cart: 'Carrito de Compras',
-  card: 'Tarjeta de Crédito',
-  addcard: 'Agregar Tarjeta de Crédito',
-  location: 'Ubicación',
-  addlocation: 'Agregar Ubicación',
-  payment: 'Método de Pago'
-}
-
 // Dynamic page component
 function DynamicPage(props) {
   // Use either passed prop or URL parameter
@@ -61,11 +46,10 @@ function DynamicPage(props) {
 
   const Component = components[componentKey] || Catalog;
 
-  // Dynamically update the document title using the titles object
+  // Set the document title to "CompraFácil" for all pages
   useEffect(() => {
-    const componentTitle = titles[componentKey] || 'CompraFácil'; // Fallback to "CompraFácil" if no title is found
-    document.title = `${componentTitle}`;
-  }, [componentKey]);
+    document.title = "CompraFácil";
+  }, []);
 
   return (
     <Background>
